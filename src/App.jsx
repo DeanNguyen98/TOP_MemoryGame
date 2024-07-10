338299a3cfae8724345e78a442916d02c37e3c7c
DeanNguyen98 <nctuan.hn@gmail.com>
2024-07-10 18:22:35 +1000

    Implement gameStart and loading state to display startscreen and loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,33 @@ import { fetchData } from './components/PokemonAPI'
 import "./App.scss";
 import MainGame from './components/MainGame';
 import EndModal from './components/EndModal';
+import StartScreen from './components/StartScreen';
+import Loading from './components/Loading';
 
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
   const [playerScore, setPlayerScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [endCondition, setEndCondition] = useState(false);
-  useEffect(() => {
-    const getPokemonData = async () => {
-      const data = await fetchData();
-      setPokemonData(data);
-    }
-    getPokemonData();
-  }, []);
-
+  const [start, setStart] = useState(false);
+  const [loading, setLoading] = useState(false)
+  
   useEffect(() => {
     if (playerScore > highScore) setHighScore(playerScore);
   }, [playerScore, highScore]);
-  
+
+  const getPokemonData = async () => {
+    setLoading(true);
+    const data = await fetchData();
+    setPokemonData(data);
+    setLoading(false);
+  }
+
+  async function gameStart() {
+    setStart(true);
+    await getPokemonData()
+
+  }
    function gameReset() {
     setPokemonData((prevPokemonData) => {
       return prevPokemonData.map(pokemon => ({
@@ -52,11 +61,20 @@ function App() {
   }
   return (
     <div className="main-container">
-       <MainGame pokemonData={pokemonData} 
+      {!start ? (
+        <StartScreen 
+        handleStartClick={gameStart}/>
+      ) : loading ? (
+        <Loading />
+      ) : (
+        <MainGame pokemonData={pokemonData} 
       playerScore={playerScore}
       highScore={highScore} 
       handleClick={handleCardClicked}
       />
+      )
+      
+    }
       {endCondition &&  
         <EndModal
         handleClick={()=> {
