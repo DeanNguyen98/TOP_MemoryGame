8e52df46650536a0ecf3b740f93c071d324b35df
DeanNguyen98 <nctuan.hn@gmail.com>
2024-07-10 18:22:08 +1000

    Create Loading screen and start screen for initlial opening of the app

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,9 @@
+export default function StartScreen ({handleStartClick}) {
+    return (
+        <>
+        <div className="start-screen">
+        <button onClick={handleStartClick}>Start</button>
+        </div>
+        </>
+    )
+}
