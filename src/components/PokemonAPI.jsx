46260e3dfbb7132b30f2024e29e466041fb81211
DeanNguyen98 <nctuan.hn@gmail.com>
2024-07-15 21:03:40 +1000

    Rewrite the API function to retrieve 9 random pokemons from a list of 150 for more pokemon variety

--- src/components/PokemonAPI.jsx
+++ src/components/PokemonAPI.jsx
@@ -1,24 +1,35 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const pokeAPI = `https://pokeapi.co/api/v2/pokemon?limit=9&offset=1000`
+const pokeAPI = `https://pokeapi.co/api/v2/pokemon?limit=150&offset=0`;
+
 export const fetchData = async () => {
-    try {
-      const response = await fetch(pokeAPI);
-      const data = await response.json();
-      const promises = data.results.map(async (pokemon) => {
-        const pokemonResponse = await fetch(pokemon.url);
-        const pokemondata = await pokemonResponse.json();
-        return {
-          name: pokemon.name,
-          url: pokemon.url,
-          image: pokemondata.sprites.front_default,
-          id: uuidv4(),
-          isClicked: false
-        }
-      })
-      const pokemonList = await Promise.all(promises);
-      return pokemonList;
-    } catch (error) {
-      console.log('Error fetching data', error);
+  try {
+    const response = await fetch(pokeAPI);
+    const data = await response.json();
+    const selectedPokemon = [];
+    while (selectedPokemon.length < 9) {
+      const randomIndex = Math.floor(Math.random() * data.results.length);
+      const selected = data.results.splice(randomIndex, 1)[0];
+      if (selected) {
+        selectedPokemon.push(selected);
+      }
     }
-  } 
+
+    const promises = selectedPokemon.map(async (pokemon) => {
+      const pokemonResponse = await fetch(pokemon.url);
+      const pokemondata = await pokemonResponse.json();
+      return {
+        name: pokemon.name,
+        url: pokemon.url,
+        image: pokemondata.sprites.front_default,
+        id: uuidv4(),
+        isClicked: false
+      }
+    });
+
+    const pokemonList = await Promise.all(promises);
+    return pokemonList;
+  } catch (error) {
+    console.log('Error fetching data', error);
+  }
+};
