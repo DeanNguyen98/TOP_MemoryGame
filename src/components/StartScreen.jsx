20c34e1ba7e07bd8a9bb042ad36e257a402dc3b6
DeanNguyen98 <nctuan.hn@gmail.com>
2024-07-16 22:16:55 +1000

    Add Modal class to StartScreen for easier grouping of styling

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,8 +1,11 @@
+import "../styles/StartScreen.scss"
 export default function StartScreen ({handleStartClick}) {
     return (
         <>
-        <div className="start-screen">
-        <button onClick={handleStartClick}>Start</button>
+        <div className="start-screen Modal">
+            <div className="start-menu">
+            <button onClick={handleStartClick}>START GAME</button>
+            </div>
         </div>
         </>
     )
