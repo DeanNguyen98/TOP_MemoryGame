8d2bf0633765a5be7598e4b081ebdfa9fac90d09
DeanNguyen98 <nctuan.hn@gmail.com>
2024-07-17 00:37:17 +1000

    Add card flip css and className for each card div

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,18 @@
-export default function Card({ pokemon, handleClick }) {
+export default function Card({ pokemon, handleClick, flip }) {
     return (
       <>
-        <div className="card">
-          <button onClick={handleClick}>
-            <img src={pokemon.image} alt={pokemon.name} className="card-image"></img>
-            <p className="card-name">{pokemon.name}</p>
-          </button>
+        <div className={`card ${flip ? "flip" : ""}`}>
+          {/* card-front */}
+          <div className="card-front">
+            <button onClick={handleClick}>
+              <img src={pokemon.image} alt={pokemon.name} className="card-image"></img>
+              <p className="card-name">{pokemon.name}</p>
+            </button>
+          </div>
+          {/*card-back*/}
+          <div className="card-back">
+            <img src="../../public/cardBack.jpg"></img>
+          </div>
         </div>
       </>
     )
