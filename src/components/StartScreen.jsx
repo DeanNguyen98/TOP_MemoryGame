f493b0002a56a794421d55145b5851801f709fe4
DeanNguyen98 <nctuan.hn@gmail.com>
2024-07-22 21:23:00 +1000

    Add instruction for game on StartScreen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -6,6 +6,10 @@ export default function StartScreen ({handleStartClick}) {
             <div className="start-menu">
             <button onClick={handleStartClick}>START GAME</button>
             </div>
+            <div className="instructions">
+            <p>Instruction: There are 9 randomly generated pokemons to catch. Try not to catch a pokemon twice. </p>
+            <p>Let&apos;s catch them all, pokemon trainer</p>
+            </div>
         </div>
         </>
     )
