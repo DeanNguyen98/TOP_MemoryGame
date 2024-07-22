b9c359ff390e1d34984316477624fe4f303e6d2b
DeanNguyen98 <nctuan.hn@gmail.com>
2024-07-22 22:17:20 +1000

    Change class Name for loading to add styling

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,11 @@
+import "../styles/Loading.scss"
+
 export default function Loading() {
     return (
-        <div className="loading">
-            ...Loading
+        <div className="loading-ctn">
+            <div className="pokemon">
+            </div>
+            <p>Finding Wild Pokemons...</p>
         </div>
     )
 }
