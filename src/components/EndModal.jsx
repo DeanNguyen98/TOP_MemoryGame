8c0c697695e3cf22d7009422240e03398f3a6544
DeanNguyen98 <nctuan.hn@gmail.com>
2024-07-22 21:57:44 +1000

    Add Gif for end Modal

--- src/components/EndModal.jsx
+++ src/components/EndModal.jsx
@@ -3,6 +3,7 @@ export default function EndModal({handleClick}) {
     return (
        <dialog open className="Modal">
             <div className="modal-content">
+            <iframe src="https://giphy.com/embed/7SF5scGB2AFrgsXP63" className="giphy-embed"></iframe>
                 <p>You lose. Try again?</p>
                 <button onClick={handleClick}>Restart</button>
             </div>
